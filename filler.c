9cf4acafcbae987745253e55234dd2c6ab8e5de6
WSeegers <wseegers.mauws@gmail.com>
2018-06-02 10:41:34 +0200

    working needs cleanup

--- filler.c
+++ filler.c
@@ -6,17 +6,12 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/31 23:59:22 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/02 09:44:58 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/06/02 10:35:06 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 #include "f_io.h"
-#include <limits.h>
-#include "f_io.h"
-#include "f_string.h"
-#include "f_print.h"
-#include "f_memory.h"
 
 int		main(void)
 {
@@ -31,10 +26,7 @@ int		main(void)
 		else
 			f_skip_line(STDIN);
 		if(parse_map(info))
-		{
-			f_print_str("\n");
-			continue ;
-		}
+			return (0);
 		parse_token(info);
 		eval_map(info);
 		eval_token(info);
